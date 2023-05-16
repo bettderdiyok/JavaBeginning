67f1b21c276a165cd934cb52502174c9529e7cd5
Betul Ekren <117034911+bettekren@users.noreply.github.com>
2023-05-16 10:51:46 +0300

    Update LoopsDoWhile.java

--- LoopsDoWhile.java
+++ LoopsDoWhile.java
@@ -1,12 +1,25 @@
 public class LoopsDoWhile {
     public static void main(String[] args) {
         //Döngü en az bir kere çalışır.
-        int i=1;
+      /*  int i=1;
         do{
             System.out.print(i + ",");
             i++;
         }
         while(i<=5);
+        */
+        
+        int i=1, j=1;
+while(i<3)
+{
+  do
+  {
+    System.out.print(j + ",");
+    j++;
+  }while(j<4);
+  i++;
+}
+
     }
 
 }
