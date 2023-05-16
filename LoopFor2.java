45b0cda2a6cbc1cb23a090427bc03656b1ee00b7
Betul Ekren <117034911+bettekren@users.noreply.github.com>
2023-05-16 09:49:12 +0300

    Update LoopFor2.java

--- LoopFor2.java
+++ LoopFor2.java
@@ -2,7 +2,7 @@
 public class LoopFor2 {
 
 	public static void main(String[] args) {
-		for(int i=0; i<10; i++) {
+		/* for(int i=0; i<10; i++) {
 			if(i==5) {
 				continue;
 			
@@ -14,7 +14,16 @@ public class LoopFor2 {
 			}
 			System.out.println(i);			
 		}
-		System.out.println(2);		
+		System.out.println(2);	
+		/*
+		int i=0;
+                for(i=1; i<=6;i++) {
+               if(i%3==0)
+               continue;
+               System.out.print(i+",");
+               }
+
+		
 
 	}
 
