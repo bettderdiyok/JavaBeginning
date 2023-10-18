f96f1c31105a1574864095ff68ae05f7e0f6118c
Betul Ekren <117034911+bettekren@users.noreply.github.com>
2023-10-18 21:37:40 +0300

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -3,29 +3,42 @@ public class Strings {
 
 	public static void main(String[] args) {
 		String str1 = "Hello Java";
-				System.out.println(str1);
-				System.out.println(str1.length()); //Uzunluğunu verir
-				System.out.println(str1.charAt(6)); //6. harfi verir.
+		String st1, st2, st3, st4, st5;
+                int a;
+		st1 = "Hello!";
+                st2 = " Java is ";
+                st3 = " Learning";
+                st4 = "Easy";
+                st5 = st1.concat(st3).concat(st2).concat(st4);
 				
-				//length bir metod bu yüzden () ile bitmesi gerek
-				int length = str1.length();
-				System.out.println(length);
+		System.out.println(str1);
+	        System.out.println(str1.length()); //Uzunluğunu verir
+		System.out.println(str1.charAt(6)); //6. harfi verir.
 				
-				String str2 = "Hello Java" + "Java is the best"	;	
-				System.out.println(str2.length());
+		//length bir metod bu yüzden () ile bitmesi gerek
+		int length = str1.length();
+		System.out.println(length);
+				
+		String str2 = "Hello Java" + "Java is the best"	;	
+		System.out.println(str2.length());
 				
 				
-				String str3, str4 , str5;
+		String str3, str4 , str5;
 				
-				str3 = "Hello";
-				str4 = "Java";
-				str5 = str3 + str4;
-				/*
-				System.out.println(str5.length());
-	üstteki + ile birleştirme yerine String de concat metodu kullanılır */
+		str3 = "Hello";
+		str4 = "Java";
+		str5 = str3 + str4;
+		/*
+		System.out.println(str5.length());
+	        üstteki + ile birleştirme yerine String de concat metodu kullanılır */
 				
-				str5 = str3.concat(str4);
-				System.out.println(str5 + "-" + str5.length());
+		str5 = str3.concat(str4);
+		System.out.println(str5 + "-" + str5.length());
+		
+                
+
+        System.out.println(st5);
+        System.out.println(st5.length());
 				
 
 	}
