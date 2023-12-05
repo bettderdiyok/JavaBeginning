9e026ce12a68d3ee0afd39236a660bc18ac22369
Betül Ekren <117034911+bettekren@users.noreply.github.com>
2023-12-05 21:12:54 +0300

    Create Foreach.java

---
+++ Foreach.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+      /* int[] array = {1, 4, 3, 5, 6, 4, 6};
+      for(int x:array){
+          System.out.print(x + " ");
+      }
+*/
+       /* String[] str = {"Betul", "Selim", "Arda", "Melek"};
+        for(String i:str){
+            System.out.print(i + " ");
+
+        }
+
+        */
+
+      /*  int[][] matrix = {{1, 2, 3},
+                          {4, 5, 6},
+                          {7, 8, 9},
+                          {10, 11, 12}
+        };
+
+        for(int[] row: matrix){
+            for(int column : row){
+                System.out.print(column + " ");
+
+
+            }
+
+
+        }
+
+       */
+        int[] scores = new int[10];
+        scores = new int[]{215, 234, 218, 189, 221, 290}; // Bu kullanım da mevcuttur.
+    }
+}
